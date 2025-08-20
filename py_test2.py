e15003b33a44d6b60c93a4f10cb1e50fd6835112
chenbrilliancesol <chenbrilliancesol@gmail.com>
2025-08-20 22:00:34 +0800

    Add files via upload

---
+++ py_test2.py
@@ -0,0 +1,62 @@
+# i=100
+# for i in range(100,999):
+#     b=i//100
+#     s=(i-100*b)//10
+#     g=(i%100)%10
+#     n =b**3+s**3+g**3
+#     if n==i:
+#         print(f'{i}是水仙数')
+#     i+=i
+#
+#
+# for i in range(3):
+#     user = input('请输入账号')
+#     if user=='itcast':
+#         password = input('请输入密码')
+#         if password=='hmai':
+#             print('登录成功')
+#         else:
+#             print(f'密码错误,你还有{2-i}次机会')
+#     else:
+#         print(f'账号错误,你还有{2-i}次机会')
+
+
+# string ='1qa2ws3ed'
+# string2 =string.find('2')
+# print(string2)
+# string3 =string.index('2')
+# print(string3)
+# string4 =string.replace('2','4',2)
+# print(string4)
+# string5 =string.split('2')
+# print(string5)
+# string7='Q,W'
+# string6 =string4.join([1,2,3])
+# print(string6)
+# print(type(string6))
+# print(type(string5))
+
+
+# sum=0
+# for i in range(1,10,2):
+#     sum=sum+i
+#     print(sum)
+
+
+# string='0123456789'
+# print(string[9:5:-1])
+
+# list1 = ['Tom', 'Rose', 'Jack']
+# # 1、使用extend方法追加元素"Jennify"
+# names = ['Tom', 'Rose', 'Jack']
+# # names.extend("Jennify")
+# # print(names)
+# #
+# # # 2、建议：使用extend方法两个列表进行合并
+# # list2 = ['Hack', 'Jennify']
+# # list1.extend(list2)
+# #
+# # print(list1)
+#
+# print('Tom'in names)
+
